0c34701856732f931efdcec25419c87a10ecd1b3
Oluwaseyi David <oluwaseyidavid26@gmail.com>
2026-05-06 03:16:22 -0700

    last-last

--- components/About/AboutHero.tsx
+++ components/About/AboutHero.tsx
@@ -57,7 +57,7 @@ export default function AboutHero() {
               style={{ height: "520px" }}
             >
               <img
-                src="https://images.unsplash.com/photo-1504307651254-35680f356dfd?w=900&q=80"
+                src="/about-img.webp"
                 alt="Gree Iloson operations"
                 className="w-full h-full object-cover"
               />
